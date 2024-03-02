a1667129812cd492164a67bf458f72e2a34732dd
Alex B <alex.bezdetny@eclape.com>
2024-03-02 08:36:37 +0100

    e2e tests and debugging

--- apps/auth/src/users/models/user.schema.ts
+++ apps/auth/src/users/models/user.schema.ts
@@ -7,6 +7,8 @@ export class UserDocument extends AbstractDocument {
   email: string;
   @Prop()
   password: string;
+  @Prop()
+  roles?: string[];
 }
 
 export const UserSchema = SchemaFactory.createForClass(UserDocument);
